76b63744ea33482739fa6430e02686ef689b62be
Uday Kumar <udayrk19@gmail.com>
2024-09-26 23:59:54 -0500

    Array problems

--- src/arrays/StockBuySellProblem.java
+++ src/arrays/StockBuySellProblem.java
@@ -2,18 +2,18 @@ package arrays;
 
 public class StockBuySellProblem {
 
-    public static void maxProfit(int[] price, int n){
+    public static int maxProfit(int[] price, int n){
         int maxPr = 0;
-        int start = price[0];
-        for(int i=1; i<n-1; i++){
-            if(price[n+1] < price[n]){
-                maxPr = price[n] - start;
-                start = price[n+1];
+        for(int i=1; i<n; i++){
+            if(price[i-1] < price[i]){
+                maxPr += price[i] - price[i - 1];
             }
         }
+        return maxPr;
     }
     public static void main(String[] args) {
         int[] price = { 100, 180, 260, 310, 40, 535, 695 };
         int n = price.length;
+        System.out.println(maxProfit(price, n));
     }
 }
